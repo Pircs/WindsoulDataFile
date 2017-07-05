1307d39aef58abd2712429438f30e8eac5afa9a8
Filipe Gomes Peixoto <gomespeixoto.filipe@gmail.com>
2017-07-05 20:06:29 +0200

    Base solution

---
+++ src/WindsoulDataFile.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WindsoulDataFile.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
